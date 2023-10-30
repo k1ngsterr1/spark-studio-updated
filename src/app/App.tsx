5c2b4c6d3efef1a83ec311dc79bf58e25d1d23b7
Ruslan Makhmatov <ruslanmakhmatov@gmail.com>
2023-10-30 23:46:41 +0600

    Version 1.0.1

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,9 +1,4 @@
-import {
-  BrowserRouter as Router,
-  Route,
-  Routes,
-  useLocation,
-} from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
