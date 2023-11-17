56b616e1fb0e5ce1c7bf726ca57c2bd075408502
Ruslan Makhmatov <ruslanmakhmatov@gmail.com>
2023-11-17 15:45:32 +0600

    Version 1.2.8

---
+++ src/features/Home/ui/PortfolioFeature/index.tsx
@@ -0,0 +1,20 @@
+import { Slide } from "react-awesome-reveal";
+
+export const PortfolioFeature = () => {
+  return (
+    <>
+      <Slide direction="left" triggerOnce={true}>
+        <h4 className="mt-20">
+          Наше <span className="orange">портфолио</span>
+        </h4>
+      </Slide>
+      <Slide direction="right" triggerOnce={true}>
+        <p className="mt-4">
+          Идейные соображения высшего порядка, а также консультация с широким
+          активом позволяет оценить значение дальнейших направлений развития. Не
+          следует, однако забывать, что реализация.
+        </p>
+      </Slide>
+    </>
+  );
+};
