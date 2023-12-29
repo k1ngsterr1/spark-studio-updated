5935817451fcd4481f68f57a9d748a43859b13a4
Ruslan Makhmatov <ruslanmakhmatov@gmail.com>
2023-12-29 18:47:06 +0600

    Version 1.4.9

--- src/features/Screens/WorkScreen/index.tsx
+++ src/features/Screens/WorkScreen/index.tsx
@@ -1,15 +1,72 @@
+import { WorkFlowTab } from "@shared/ui/WorkFlow";
+import { PortfolioFeature } from "@features/Home/ui/PortfolioFeature";
+
 import { Number } from "@shared/ui/Number";
+import line from "@assets/images/line.svg";
 
 import "./styles.scss";
 
 export const WorkScreen = () => {
   return (
-    <div className="main mt-16">
+    <div className="main m-auto flex items-center mt-16">
       <h3 className="text-center">
         Процесс <span className="orange">Работы</span>
       </h3>
       <div className="work-container">
-        <Number number="01" marginTop="mt-4" color="text-custom-orange" />
+        <WorkFlowTab
+          tabName="Брифинг и техническое задание"
+          tabAdditionalText="Для четкого понимания поставленной задачи, работа над веб-сайтом начинается с опроса клиента и заполнения брифа. Для более сложных проектов составляется техническое задание."
+          tabUnderText="1-2 Дня"
+          tabNumber="01"
+          numberColor="text-custom-orange"
+          marginTop="mt-4"
+          containerMarginTop="mt-12"
+          bgImage="editor"
+        />
+        <WorkFlowTab
+          tabName="Дизайн и
+          прототипирование"
+          tabAdditionalText="На основе брифа либо техзадания, команда дизайнеров создает интерактивный дизайн-макет и прототип будущего сайта. На этой стадии разработки с клиентом обсуждается и согласовывается визуальная составляющая, интерфейс и пользовательский опыт."
+          tabUnderText="2-7 Дней"
+          tabNumber="02"
+          numberColor="text-custom-black"
+          marginTop="mt-4"
+          containerMarginTop="mt-12"
+          bgImage="design"
+        />
+        <WorkFlowTab
+          tabName="Вёрстка
+          и программирование"
+          tabAdditionalText="После согласования дизайн-макета за работу принимается команда разработчиков. Фронтенд и бэкенд специалисты воплащают дизайн в рабочее решение с главным приоритетом - максимально быстрой загрузки сайта."
+          tabUnderText="2-7 Дня"
+          tabNumber="03"
+          numberColor="text-custom-orange"
+          marginTop="mt-4"
+          containerMarginTop="mt-12"
+          bgImage="code"
+        />
+        <WorkFlowTab
+          tabName="Наполнение сайта
+          контентом"
+          tabAdditionalText="После согласования дизайн-макета за работу принимается команда разработчиков. Фронтенд и бэкенд специалисты воплащают дизайн в рабочее решение с главным приоритетом - максимально быстрой загрузки сайта."
+          tabUnderText="2-7 Дня"
+          tabNumber="04"
+          numberColor="text-custom-black"
+          marginTop="mt-4"
+          containerMarginTop="mt-12"
+          bgImage="content"
+        />
+        <WorkFlowTab
+          tabName="Тестирование и перенос
+          на постоянный хостинг"
+          tabAdditionalText="По завершении разработки мы переносим сайт на постоянный хостинг, проводим различные тестирования сайта на нагрузки, стабильную работу всего функционала, выявляем и устраняем возможные ошибки."
+          tabUnderText="1 День"
+          tabNumber="05"
+          numberColor="text-custom-orange"
+          marginTop="mt-4"
+          containerMarginTop="mt-12"
+          bgImage="settings"
+        />
       </div>
     </div>
   );
---
+++ src/shared/ui/WorkFlow/index.tsx
@@ -0,0 +1,50 @@
+import { TypeAnimation } from "react-type-animation";
+import { Number } from "../Number";
+
+import line from "@assets/images/line.svg";
+
+interface WorkFlowProps {
+  tabName: string;
+  tabNumber: string;
+  tabAdditionalText: string;
+  tabUnderText: string;
+  numberColor: string;
+  marginTop: string;
+  containerMarginTop: string;
+  bgImage: string;
+}
+
+export const WorkFlowTab: React.FC<WorkFlowProps> = ({
+  tabName,
+  tabNumber,
+  tabUnderText,
+  numberColor,
+  marginTop,
+  containerMarginTop,
+  tabAdditionalText,
+  bgImage,
+}) => {
+  return (
+    <>
+      <div className={`work-container ${bgImage}`}>
+        <div
+          className={`work-container__image-container ${containerMarginTop}`}
+        >
+          <img
+            src={line}
+            className="work-container__image-container__line"
+            alt="line"
+          />
+          <span className="work-container__image-container__text">
+            {tabUnderText}
+          </span>
+        </div>
+        <Number number={tabNumber} marginTop={marginTop} color={numberColor} />
+        <span className="work-container__text">{tabName}</span>
+        <span className="work-container__additional-text">
+          <TypeAnimation sequence={[tabAdditionalText]}></TypeAnimation>
+        </span>
+      </div>
+    </>
+  );
+};
