3baf46547a1d342e0cc84561c56c7925da00a36d
Ruslan Makhmatov <ruslanmakhmatov@gmail.com>
2024-02-02 20:17:33 +0600

    Version 2.0.1

--- src/shared/lib/data/portfolioContent.ts
+++ src/shared/lib/data/portfolioContent.ts
@@ -16,6 +16,16 @@ import AS_MOCKUP from "@assets/images/sites/mockups/AS_MOCKUP.png";
 import AS_PHONE_MOCKUP from "@assets/images/sites/mockups/AS_PHONE_MOCKUP.png";
 import AS_MAIN from "@assets/images/sites/mockups/AS_MAIN_IMAGE.png";
 
+// Cargo
+import CARGO_MOCKUP from "@assets/images/sites/mockups/CARGO_MOCKUP.png";
+import CARGO_MAIN from "@assets/images/sites/mockups/CARGO_MAIN_IMAGE.png";
+import CARGO_PHONE_MOCKUP from "@assets/images/sites/mockups/CARGO_PHONE_MOCKUP.png";
+
+// SHBT
+import SHBT_MOCKUP from "@assets/images/sites/mockups/SHBT_MOCKUP.png";
+import SHBT_MAIN from "@assets/images/sites/mockups/SHBT_MAIN_IMAGE.png";
+import SHBT_PHONE_MOCKUP from "@assets/images/sites/mockups/SHBT_PHONE_MOCKUP.png";
+
 export interface PhotoItem {
   photo: string;
 }
@@ -116,6 +126,100 @@ export const portfolioContent: Record<string, PortfolioInfo> = {
     metaTitle: "Проверка",
     metaDescription: "Проверка",
     phoneMockup: AS_PHONE_MOCKUP,
+    adaptiveDescription: [
+      {
+        text: "Сайт спроектирован таким образом, что-бы соответствовать современным требованиям юзабилити. По умолчанию сайт интернет-магазина проектируется не только в десктопной версии, но и под мобильные устройства (планшет и смартфон).",
+      },
+      {
+        text: "Сайт спроектирован таким образом, что-бы соответствовать современным требованиям юзабилити. По умолчанию сайт интернет-магазина проектируется не только в десктопной версии, но и под мобильные устройства (планшет и смартфон).",
+      },
+      {
+        text: "Сайт спроектирован таким образом, что-бы соответствовать современным требованиям юзабилити. По умолчанию сайт интернет-магазина проектируется не только в десктопной версии, но и под мобильные устройства (планшет и смартфон).",
+      },
+    ],
+    // gallery: [
+    //   {
+    //     photo: photo,
+    //   },
+    // ],
+    options: [
+      {
+        icon: faReact,
+        list: "React",
+      },
+      {
+        icon: faGit,
+        list: "Git",
+      },
+      {
+        icon: faFigma,
+        list: "Figma",
+      },
+    ],
+  },
+  cargo: {
+    title: "Cargo Express PV",
+    taskDescription:
+      "Разработка интернет-магазина полного цикла дизайн-макеты страниц, верстка, адаптивные версии под мобильные устройства, интеграция с Битрикс и платежными системами.",
+    subTitle: "Сайт для проектно-архитектурной компании",
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+    techStackTitle: "Технологический стэк",
+    mainImage: CARGO_MAIN,
+    backgroundImage: "CARGO_BG",
+    logo: SS_LOGO,
+    mockUpImage: CARGO_MOCKUP,
+    linkProject: "https://cargopv.kz/",
+    metaTitle: "Проверка",
+    metaDescription: "Проверка",
+    phoneMockup: CARGO_PHONE_MOCKUP,
+    adaptiveDescription: [
+      {
+        text: "Сайт спроектирован таким образом, что-бы соответствовать современным требованиям юзабилити. По умолчанию сайт интернет-магазина проектируется не только в десктопной версии, но и под мобильные устройства (планшет и смартфон).",
+      },
+      {
+        text: "Сайт спроектирован таким образом, что-бы соответствовать современным требованиям юзабилити. По умолчанию сайт интернет-магазина проектируется не только в десктопной версии, но и под мобильные устройства (планшет и смартфон).",
+      },
+      {
+        text: "Сайт спроектирован таким образом, что-бы соответствовать современным требованиям юзабилити. По умолчанию сайт интернет-магазина проектируется не только в десктопной версии, но и под мобильные устройства (планшет и смартфон).",
+      },
+    ],
+    // gallery: [
+    //   {
+    //     photo: photo,
+    //   },
+    // ],
+    options: [
+      {
+        icon: faReact,
+        list: "React",
+      },
+      {
+        icon: faGit,
+        list: "Git",
+      },
+      {
+        icon: faFigma,
+        list: "Figma",
+      },
+    ],
+  },
+  sharbakty: {
+    title: "Sharbakty Bidai Terminal",
+    taskDescription:
+      "Разработка интернет-магазина полного цикла дизайн-макеты страниц, верстка, адаптивные версии под мобильные устройства, интеграция с Битрикс и платежными системами.",
+    subTitle: "Сайт для проектно-архитектурной компании",
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+    techStackTitle: "Технологический стэк",
+    mainImage: SHBT_MAIN,
+    backgroundImage: "SHBT_BG",
+    logo: SS_LOGO,
+    mockUpImage: SHBT_MOCKUP,
+    linkProject: "https://cargopv.kz/",
+    metaTitle: "Проверка",
+    metaDescription: "Проверка",
+    phoneMockup: SHBT_PHONE_MOCKUP,
     adaptiveDescription: [
       {
         text: "Сайт спроектирован таким образом, что-бы соответствовать современным требованиям юзабилити. По умолчанию сайт интернет-магазина проектируется не только в десктопной версии, но и под мобильные устройства (планшет и смартфон).",
